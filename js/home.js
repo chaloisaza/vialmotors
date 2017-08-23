6d1709fa21a8ac26715e652affe85374196ea6c6
chaloisaza <chaloisaza@gmail.com>
2017-08-23 01:45:52 -0500

    Add navigation

--- js/home.js
+++ js/home.js
@@ -1,5 +1,13 @@
 (function ($) {
     $(document).ready(function () {
+        //init vars
+        $('#bannerImage').hide();
+        $('.divCategories').hide();
+        $('#searchDiv').hide();
+
+
+
+        //start scroll back top function
         $(window).scroll(function () {
             if ($(this).scrollTop() > 50) {
                 $('#back-to-top').fadeIn();
@@ -7,6 +15,7 @@
                 $('#back-to-top').fadeOut();
             }
         });
+
         // scroll body to 0px on click
         $('#back-to-top').click(function () {
             $('body,html').animate({
@@ -15,6 +24,7 @@
             return false;
         });
 
+        //start owl owlCarousel
         var owl = $('.owl-carousel');
         owl.owlCarousel({
             items: 6,
@@ -45,6 +55,72 @@
                     items: 6,
                 }
             }
+        });
+
+
+        //logic for nav menu
+        $('.navbarList').click(function (e) {
+
+
+            //hide search div
+            $('#searchDiv').hide();
+
+            //remove nav active class
+            $('.navbarList').removeClass('active');
+            //add nav specific class
+            $('#' + e.target.id).parent().addClass('active');
+
+            //remove list-group-item active class
+            $('.list-group-item').removeClass('active');
+
+
+            //hide all categorie div and show specif div
+
+            $('.divCategories').hide();
+            $('#div' + e.target.id).show();
+
+            //show carousel if home else hide
+            if (e.target.id == "home") {
+                $('#divInicio').show();
+                $('#carouselExampleIndicators').show();
+            }
+            else {
+                $('#carouselExampleIndicators').hide();
+            }
+
+            //hide banner categories
+            $('#bannerImage').hide();
+        });
+
+        //logic for categories menu
+        $('.list-group-item').click(function (e) {
+
+            //show search div
+            $('#searchDiv').show();
+
+            //remove categories active class
+            $('.list-group-item').removeClass('active');
+
+            //add categories specific class
+            $('#' + e.target.id).addClass('active');
+
+            //remove navbar active class
+            $('.navbarList').removeClass('active');
+
+            //change carousel for specific banner
+            $('#carouselExampleIndicators').hide();
+            $('#bannerImage').show();
+            $('#bannerImage').css("background", "url(images/categories/" + e.target.id + ".jpg)");
+
+            //hide all categorie div and show specif div
+            $('#divInicio').hide();
+            $('.divCategories').hide();
+            $('#div' + e.target.id).show();
+
+
+            $('body,html').animate({
+                scrollTop: $('#div' + e.target.id).offset().top
+            }, 800);
         });
     });
 })(jQuery);
